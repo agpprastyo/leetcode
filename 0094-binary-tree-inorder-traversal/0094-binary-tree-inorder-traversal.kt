ff467da0dfcc13271bd6ab4dbb07e091b7ef87e1
Agung Prasetyo <prasetyo.agpr@gmail.com>
2024-07-30 11:04:59 +0700

    Time: 145 ms (75.27%), Space: 34 MB (79.89%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.kt
@@ -0,0 +1,30 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val result = mutableListOf<Int>()
+        val stack = mutableListOf<TreeNode>()
+        var current = root
+
+        while (current != null || stack.isNotEmpty()) {
+            while (current != null) {
+                stack.add(current)
+                current = current.left
+            }
+
+            current = stack.removeAt(stack.size - 1)
+            result.add(current.`val`)
+            current = current.right
+        }
+
+        return result
+    }
+}
