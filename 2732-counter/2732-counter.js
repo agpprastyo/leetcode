021ea4a2358f32442dc905244987d8866c710fb6
Agung Prasetyo <prasetyo.agpr@gmail.com>
2024-07-30 11:23:41 +0700

    Time: 50 ms (62.03%), Space: 48.9 MB (44.05%) - LeetHub

--- 2732-counter/2732-counter.js
+++ 2732-counter/2732-counter.js
@@ -3,12 +3,9 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    // Initialize the counter with the starting value n
-    let current = n;
-    
-    // Return a function that increments and returns the current value
+   
     return function() {
-        return current++;
+        return n++;
     };
 };
 
