f3209033c08d7e4e46350b50604d112dad73a463
Agung Prasetyo <prasetyo.agpr@gmail.com>
2025-08-20 14:07:22 +0700

    Time: 2 ms (78.11%) | Memory: 4.5 MB (62.66%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.go
@@ -0,0 +1,34 @@
+func longestPalindrome(s string) string {
+    if len(s) < 1 {
+        return ""
+    }
+
+    start := 0
+    end := 0
+
+    for i := 0; i < len(s); i++ {
+        len1 := expandAroundCenter(s, i, i)
+        len2 := expandAroundCenter(s, i, i + 1)
+
+        maxLen := len1 
+        if len2 > len1 {
+            maxLen = len2
+        }
+
+        if maxLen > (end - start) {
+            start = i - (maxLen-1)/2
+            end = i + maxLen / 2
+        }
+        
+    }
+    return s[start : end + 1]
+}
+
+
+func expandAroundCenter(s string, left int, right int) int {
+    for left >= 0 && right < len(s) && s[left] == s[right] {
+        left--
+        right++
+    }
+    return right - left - 1
+}
