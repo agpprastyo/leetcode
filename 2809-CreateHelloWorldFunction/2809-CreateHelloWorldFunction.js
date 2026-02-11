e050fac3b8f519cae3d26e805e8bba202ae553fd
Agung Prasetyo <prasetyo.agpr@gmail.com>
2026-02-11 20:30:10 +0700

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 2/11/2026, 8:30:10 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
