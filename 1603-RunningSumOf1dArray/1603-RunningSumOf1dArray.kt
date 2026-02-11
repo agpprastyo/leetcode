e10a9d20e71916bbce60c353e22053108caf2560
Agung Prasetyo <prasetyo.agpr@gmail.com>
2026-02-11 20:30:16 +0700

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.kt
@@ -0,0 +1,11 @@
+// Last updated: 2/11/2026, 8:30:15 PM
+class Solution {
+    fun runningSum(nums: IntArray): IntArray {
+       var i = 1
+        while(i < nums.count()) {
+            nums[i] += nums[i-1]
+            i++
+        }
+        return nums
+    }
+}
