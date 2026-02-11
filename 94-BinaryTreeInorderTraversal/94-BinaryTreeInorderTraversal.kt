3e3aa90fb6146d5be9d63e950f4db1989aadda9c
Agung Prasetyo <prasetyo.agpr@gmail.com>
2026-02-11 20:30:27 +0700

    Create 94-BinaryTreeInorderTraversal

---
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.kt
@@ -0,0 +1,32 @@
+// Last updated: 2/11/2026, 8:30:27 PM
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val result = mutableListOf<Int>()
+        val stack = Stack<TreeNode>()
+        var current = root
+
+        while (current != null || stack.isNotEmpty()) {
+            while (current != null) {
+                stack.add(current)
+                current = current.left
+            }
+
+            current = stack.pop()
+        result.add(current.`val`)
+    
+        current = current.right
+        }
+
+        return result
+    }
+}
