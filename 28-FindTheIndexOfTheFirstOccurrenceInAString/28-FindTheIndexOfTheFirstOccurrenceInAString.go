66320d507e53c1edc842136b133c4b5124a6df68
Agung Prasetyo <prasetyo.agpr@gmail.com>
2026-02-11 20:30:34 +0700

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.go
@@ -0,0 +1,16 @@
+// Last updated: 2/11/2026, 8:30:33 PM
+func strStr(haystack string, needle string) int {
+    if len(needle) == 0 {
+        return 0
+    }
+
+    haystackLen := len(haystack)
+    needleLen := len(needle)
+
+    for i := 0; i<= haystackLen-needleLen; i++ {
+        if haystack[i:i+needleLen] == needle {
+            return i
+        }
+    }
+    return -1
+}
