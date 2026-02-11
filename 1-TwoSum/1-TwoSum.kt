9698cdb2637c2a7e3a5980eefed1452c8a258982
Agung Prasetyo <prasetyo.agpr@gmail.com>
2026-02-11 20:30:57 +0700

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.kt
@@ -0,0 +1,21 @@
+// Last updated: 2/11/2026, 8:30:57 PM
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+     
+    val result = IntArray(2)
+  
+    val map: MutableMap<Int, Int> = HashMap()
+   
+    for (i in nums.indices) {
+        if (map.containsKey(nums[i])) {
+         
+            result[0] = i
+            result[1] = map[nums[i]]!!
+            break
+        } else {
+            map[target - nums[i]] = i
+        }
+    }
+    return result
+    }
+}
